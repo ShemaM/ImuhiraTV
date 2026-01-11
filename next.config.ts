3e3534b083a6124a64ed8ebe1cf00eb3112178dd
Shema Manasseh <shemamanase992@gmail.com>
2026-01-11 21:23:26 +0300

    Fix next-i18next config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
+import i18nextConfig from "./next-i18next.config.js";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  i18n: i18nextConfig.i18n,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-        port: '',
-        pathname: '/vi/**',
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+        pathname: "/vi/**",
       },
     ],
   },
